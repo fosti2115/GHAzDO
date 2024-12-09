50d853708f5667d4edf69eef576c68baa66c24a2
fosti2115 <mateusz.majstrak@edu.uekat.pl>
2024-12-09 12:42:26 +0100

    Update Google_GoogleCloudPrivateKeyId.py

--- Google_GoogleCloudPrivateKeyId.py
+++ Google_GoogleCloudPrivateKeyId.py
@@ -2,7 +2,7 @@ from google.cloud import storage
 from google.oauth2 import service_account
 
 # Ustawienie poświadczeń
-private_key_id = 'XXX'
+private_key_id = '30a803f62a5184bc69fe46e20b00a2f7f6bf06b1'
 
 credentials = service_account.Credentials.from_service_account_info(
     {'private_key_id': private_key_id, 'other_fields': 'XXX'}
