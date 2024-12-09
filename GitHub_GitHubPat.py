30876f66149fe38e3edcfe4f360cd4e02fffaf0b
fosti2115 <mateusz.majstrak@edu.uekat.pl>
2024-12-09 12:18:18 +0100

    Update and rename GitHub_GitHubPat to GitHub_GitHubPat.py

--- GitHub_GitHubPat.py
+++ GitHub_GitHubPat.py
@@ -1,6 +1,6 @@
 import requests
 
-personal_access_token = "XXX"
+personal_access_token = "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"
 
 headers = {
     "Authorization": f"token {personal_access_token}",
